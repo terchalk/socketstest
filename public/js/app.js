9c1db47ed1720746a91a39f56b0f94e55637dd01
terchalk <terencechalk@hotmail.com>
2016-12-28 14:17:21 +0800

    added time stamps

--- public/js/app.js
+++ public/js/app.js
@@ -5,13 +5,14 @@ socket.on('connect', function() {
 });
 
 socket.on('message', function(message) {
+	var momentTimestamp = moment.utc(message.timestamp);
 	console.log('message: ');
 	console.log(message.text);
 
-	jQuery('.messages').append('<p>' + message.text + "</p>");
+	jQuery('.messages').append('<p><strong>' + momentTimestamp.local().format('h:mm a') + '</strong> ' + message.text + "</p>");
 });
 
-//Handles message submission
+//Handles message submission 
 
 var $form = jQuery('#message-form');
 
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var moment = require('moment');
 
 app.use(express.static(__dirname + '/public'));
 io.on('connection', function(socket) {
@@ -10,12 +11,14 @@ io.on('connection', function(socket) {
 
 	socket.on('message', function(message) {
 		console.log('Message received: ' + message.text);
+		message.timestamp = moment().valueOf(); 
 
 		io.emit('message',message);
 	})
 
 	socket.emit('message', {
-		text: 'Welcome to terftw chat'
+		text: 'Welcome to terftw chat',
+		timestamp: moment().valueOf()
 	});
 });
 
